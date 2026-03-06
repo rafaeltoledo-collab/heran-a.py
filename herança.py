7aa35ad6ac79f64bc09af82be376427e64d1e913
rafaeltoledo-collab <rafaeltoledo@colegiopaulodetarso.com.br>
2026-03-06 13:48:40 -0300

    Fix variable name from 'raça' to 'raca'

--- herança.py
+++ herança.py
@@ -2,42 +2,42 @@ class Animal():
     def __init__(self, nome, cor, raça):
         self.__nome = nome
         self.__cor = cor 
-        self.__raça = raça
+        self.__raca = raca
 
 
     def comer(self):    
         print(f"O {self.__nome} está comendo!") 
         
     def raça(self):
-       print(f"E sua raça é:{self.__raça}")
+       print(f"E sua raca é:{self.__raca}")
 
 class gato(Animal): 
-    def __init__(self, nome, cor, raça):
-     super().__init__(nome, cor, raça)
+    def __init__(self, nome, cor, raca):
+     super().__init__(nome, cor, raca)
     
 
 class cachorro(Animal):
-    def __init__(self, nome, cor, raça):
-     super().__init__(nome, cor, raça)
+    def __init__(self, nome, cor, raca):
+     super().__init__(nome, cor, raca)
     
 
 class coelho(Animal):
-    def __init__(self, nome, cor, raça):
-     super().__init__(nome, cor, raça)
+    def __init__(self, nome, cor, raca):
+     super().__init__(nome, cor, raca)
 
 
 ramona = gato("ramona", "preto", "siames")
 ramona.comer()
-ramona.raça()
+ramona.raca()
 
 chocolate = gato("chocolate", "roxo", "persa")
 chocolate.comer()
-chocolate.raça()
+chocolate.raca()
 
 tobi = cachorro("tobi", "caramelo", "viralata")
 tobi.comer()
-tobi.raça()
+tobi.raca()
 
 maya = cachorro("maya", "preto", "rottweiler")
 maya.comer()
-maya.raça()
+maya.raca()
